4da803275e1f561eae9c553aa94a2cdc9498bcf0
mfazari <massimofazari@bluewin.ch>
2020-02-14 15:20:37 +0100

    weather function more

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from flask import Flask, jsonify, redirect, url_for, request
 import os
+import json
+import urllib.request as request
 app = Flask(__name__)
 
 
 # global variables
 status = "undefined"
+api_key = os.environ['api_key'];
 
 
 # Routes
+# TODO add routes for error
 @app.route('/forecast/<city>', methods=['GET'])
 def weather(city):
+    link = "api.openweathermap.org/data/2.5/weather?q={" + city + "}&appid={" + api_key + "}"
+    with request.urlopen(link) as response:
+        if response.getcode() == 200:
+            source = response.read()
+            data = json.loads(source)
+        else:
+            print('An error occurred while attempting to retrieve data from the API.')
     return jsonify(
         clouds="",
         humidity="",
@@ -35,12 +46,13 @@ def get_version():
     return version_number
 
 
-# Returns Status Number from Version file
+# Sets Status
 def set_status(argument):
     status = argument
 
+#
 
-# Running our little server
+# Running our little program
 if __name__ == '__main__':
     print(os.environ['public_key'])
     app.run(debug=True, port=8080)
