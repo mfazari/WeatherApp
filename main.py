49ef54f5a12f5fc7ed38ee78ed00f1a4a967fae5
mfazari <massimofazari@bluewin.ch>
2020-02-14 14:57:05 +0100

    functions added

--- main.py
+++ main.py
@@ -1,17 +1,51 @@
-from flask import Flask, redirect, url_for, request
-
-app = Flask(__name__)
+from flask import Flask, jsonify, redirect, url_for, request
 import os
+app = Flask(__name__)
 
 
-@app.route('/success/<name>')
-def success(name):
-    return 'welcome %s' % name
+# global variables
+status = "undefined"
 
 
 # Routes
-@app.route('/login', methods=['GET'])
-def login():
+@app.route('/forecast/<city>', methods=['GET'])
+def weather(city):
+    return jsonify(
+        clouds="",
+        humidity="",
+        pressure="",
+        temperature=""
+    )
+
+
+@app.route('/ping', method=['GET'])
+def ping():
+    return jsonify(
+        name="weatherservice",
+        status=status,
+        version=get_version()
+    )
+
+
+# Returns Version Number from Version file
+def get_version():
+    k = open('VERSION')
+    version_number = k.readline()
+    k.close()
+    return version_number
+
+
+# Returns Status Number from Version file
+def set_status(argument):
+    status = argument
+
+
+# Running our little server
+if __name__ == '__main__':
+    print(os.environ['public_key'])
+    app.run(debug=True, port=8080)
+
+'''
     if request.method == 'POST':
         user = request.form['nm']
         return redirect(url_for('success', name=user))
@@ -20,8 +54,8 @@ def login():
         return redirect(url_for('success', name=user))
 
 
-# Running our little server
-if __name__ == '__main__':
-    print(os.environ['public_key'])
-    app.run(debug=True, port=8080)
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' % name
 
+'''
