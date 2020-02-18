b52b4539ef6152d2257cbb9cfad657828a96aea0
mfazari <massimofazari@bluewin.ch>
2020-02-18 15:54:54 +0100

    tests added2

---
+++ test_routes.py
@@ -0,0 +1,26 @@
+# Execute file with pytest
+
+from flask import Flask
+from routes import configure_routes
+
+
+# Test existing city
+def test_weather_1():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/forecast/London'
+
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+# Test no city provided
+def test_weather_2():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/forecast/'
+
+    response = client.get(url)
+    assert response.status_code == 400
